ad3e56796080122be94ae30fc78faab0aba282f0
sekelskyp <sekelskyp@protonmail.com>
2024-12-28 23:17:43 +0100

    Move nav and footer to layout

--- sveltekit-ts/src/routes/+page.svelte
+++ sveltekit-ts/src/routes/+page.svelte
@@ -1,22 +1,5 @@
-<header>
-    <nav>
-        <a href="/card">Card</a>
-        <a href="/features">Features</a>
-        <a href="/pricing">Pricing</a>
-        <a href="/cart">Cart</a>
-        <a href="/login">Login</a>
-        <a href="/register">Register</a>
-    </nav>
-</header>
 <main>
     <section>
         <h1>Home Page</h1>
     </section>
 </main>
-<footer>
-    <nav>
-        <a href="/card">Card</a>
-        <a href="/features">Features</a>
-        <a href="/pricing">Pricing</a>
-    </nav>
-</footer>
