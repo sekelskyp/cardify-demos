49f01591b1c39049200b67c0fa6dcc9edf83595d
sekelskyp <sekelskyp@protonmail.com>
2024-12-28 22:26:31 +0100

    Add nav and footer

--- sveltekit-ts/src/routes/+page.svelte
+++ sveltekit-ts/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<header>
+    <nav>
+        <a href="#">Card</a>
+        <a href="#">Features</a>
+        <a href="#">Pricing</a>
+        <a href="#">Cart</a>
+        <a href="#">Login</a>
+        <a href="#">Sign Up</a>
+    </nav>
+</header>
+<main>
+    <section>
+        <h1>Home Page</h1>
+    </section>
+</main>
+<footer>
+    <nav>
+        <a href="#">Card</a>
+        <a href="#">Features</a>
+        <a href="#">Pricing</a>
+    </nav>
+</footer>
