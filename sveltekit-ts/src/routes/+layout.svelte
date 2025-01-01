35deb264e9284845ae5196167d5646d32573ac1f
sekelskyp <sekelskyp@protonmail.com>
2025-01-01 18:14:37 +0100

    Add layout

---
+++ sveltekit-ts/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+    let { children } = $props();
+</script>
+
+<header>
+    <nav>
+        <a href="/card">Card</a>
+        <a href="/features">Features</a>
+        <a href="/pricing">Pricing</a>
+        <a href="/cart">Cart</a>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+    </nav>
+</header>
+
+{@render children()}
+
+<footer>
+    <nav>
+        <a href="/card">Card</a>
+        <a href="/features">Features</a>
+        <a href="/pricing">Pricing</a>
+    </nav>
+</footer>
