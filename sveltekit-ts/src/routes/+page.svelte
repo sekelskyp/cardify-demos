ae5e1fe6214561665c8c9456804d9d037c327faf
sekelskyp <sekelskyp@protonmail.com>
2025-01-02 17:14:16 +0100

    Add more sections

--- sveltekit-ts/src/routes/+page.svelte
+++ sveltekit-ts/src/routes/+page.svelte
@@ -6,4 +6,45 @@
         </hgroup>
         <button>Call to action!</button>
     </section>
+    <section>
+        <hgroup>
+            <h2>Create your professional profile in a few clicks</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </hgroup>
+        <button>Reset</button>
+    </section>
+    <section>
+        <hgroup>
+            <h2>Why choose Cardify?</h2>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </hgroup>
+        <div>
+            <h2>Benefit 1</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div>
+            <h2>Benefit 2</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div>
+            <h2>Benefit 3</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div>
+            <h2>Benefit 4</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    </section>
+    <section>
+        <hgroup>
+            <h2>Easy to use dashboard for editing your profile</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </hgroup>
+    </section>
+    <section>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <p>Lorem ipsum dolor sit.</p>
+        <button>Sign Up</button>
+        <button>Order Now</button>
+    </section>
 </main>
