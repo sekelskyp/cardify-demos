4288616391b09d11d96a4ab3132624ffc424b4b2
sekelskyp <sekelskyp@protonmail.com>
2025-01-02 17:15:58 +0100

    Add page structure

--- sveltekit-ts/src/routes/card/+page.svelte
+++ sveltekit-ts/src/routes/card/+page.svelte
@@ -1,1 +1,34 @@
 <h1>Card product page</h1>
+<section>
+    <hgroup>
+        <h1>Business Card</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    </hgroup>
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br />
+        Abautem eligendi magni odio. <br />
+        Est quas ratione rem libero quos dignissimos optio id maiores reiciendis.
+    </p>
+</section>
+<section>
+    <hgroup>
+        <h2>How does it work?</h2>
+        <div>
+            <h3>Step 1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div>
+            <h3>Step 2</h3>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div>
+            <h3>Step 3</h3>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+        <div>
+            <h3>Step 4</h3>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+    </hgroup>
+</section>
+<section></section>
